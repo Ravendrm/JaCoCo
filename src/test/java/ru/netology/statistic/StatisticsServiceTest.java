4a335d79052c93fea02d446b87f1b852f4f8fd57
Ravendrm <bkforsner@gmail.com>
2022-11-22 15:18:05 +0300

    Coverage 93%

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,13 +1,12 @@
 package ru.netology.statistic;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StatisticsServiceTest {
 
     @Test
-    void findMaxA() {
+    void findMax() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
@@ -19,26 +18,17 @@ public class StatisticsServiceTest {
     }
 
     @Test
-    void findMaxE() {
-        ru.netology.statistic.StatisticsService service = new StatisticsService();
+    void findMaxNew() {
+        StatisticsService service = new StatisticsService();
 
-        long[] incomeInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 25;
+        long[] incomesInBillions = {1, 8, 4, 5, 3, 21, 6, 12, 11, 12, 12};
+        long expected = 21;
 
-        long actual = service.findMax(incomeInBillions);
+        long actual = service.findMax(incomesInBillions);
 
-        assertEquals(expected, actual);
-    }
+        Assertions.assertEquals(expected, actual);
 
-    @Test
-    void findMax() {
-        ru.netology.statistic.StatisticsService service = new StatisticsService();
 
-        long[] incomeInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 17;
-
-        long actual = service.findMax(incomeInBillions);
-
-        assertEquals(expected, actual);
     }
 }
+
